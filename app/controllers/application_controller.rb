d2b5e3978bd4af9349ab33017cd6d5fd772d4277
KaraJH <karas.jones@yahoo.com>
2023-04-22 15:43:36 -0600

    feat: Route, controller for user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid, with: :bad_request_response 
+
+  def bad_request_response(exception)
+    render json: ErrorSerializer.new(exception.message).bad_request, status: 404
+  end
 end
