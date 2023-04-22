a2676cb71502b42acf3cd79efcf29f65a08d11b0
KaraJH <karas.jones@yahoo.com>
2023-04-22 16:27:19 -0600

    feat: Route, controller made for sessions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create!(email: "whatever@example.com", password: "password1", password_confirmation: "password1")
