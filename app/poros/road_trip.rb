0ec22057c3004abcc55d30d11cd868b019c2ac8c
KaraJH <karas.jones@yahoo.com>
2023-04-23 15:56:18 -0600

    feat: Road Trip poro

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,9 @@
+class RoadTrip 
+  attr_reader :start_city, :end_city, :travel_time, :weather_at_eta
+  def initialize(start_city, end_city, travel_time, arrival_weather_info)
+    @start_city = start_city 
+    @end_city = end_city
+    @travel_time = travel_time 
+    @weather_at_eta = arrival_weather_info
+  end
+end
