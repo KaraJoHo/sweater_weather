80c6d068a009202c681b4a313fb12d85e5f34275
KaraJH <karas.jones@yahoo.com>
2023-04-24 11:34:23 -0600

    feat: Sad path, conditional added to render error if no locatio provided

--- app/controllers/api/v1/salaries_controller.rb
+++ app/controllers/api/v1/salaries_controller.rb
@@ -1,33 +1,11 @@
 class Api::V1::SalariesController < ApplicationController 
   def index 
-    # forecast_info_destination = ForecastFacade.new(params[:destination]) 
-    # lat_long = forecast_info_destination.lat_long
-    # weather_for_destination = WeatherService.new.fetch_forecast_for_given_location(lat_long.latitude, lat_long.longitude)
-    
-    # forecast = {"summary": weather_for_destination[:current][:condition][:text], "temperature": "#{weather_for_destination[:current][:temp_f].to_i} F"} 
-   
-
-    # conn = Faraday.new(url: "https://api.teleport.org")
-    # urban_city_response = conn.get("/api/urban_areas/slug:#{params[:destination]}/salaries")
-    # parsed =JSON.parse(urban_city_response.body, symbolize_names: true)
-
-    # tech_jobs = ["DATA-ANALYST", "DATA-SCIENTIST", "MOBILE-DEVELOPER", "QA-ENGINEER", "SOFTWARE-ENGINEER", "SYSTEMS-ADMINISTRATOR", "WEB-DEVELOPER"]
-   
-    # wanted_list = parsed[:salaries].reject! do |job|
-    #   !tech_jobs.include?(job[:job][:id])
-    # end
-
-    # tech_salaries = wanted_list.map do |job|
-    #   SalaryInfo.new(job)
-    # end
-
-    # salaries = tech_salaries.map do |salary|
-    #   {"title": salary.title, "min": salary.min, "max": salary.max}
-    # end
-
-    # salary_info_for_destination = Salaries.new(params[:destination], forecast, salaries)
-    salary_info_for_destination = SalariesFacade.new(params[:destination])
-    render json: SalariesSerializer.new(salary_info_for_destination.salaries_for_destination)
-
+  #  require 'pry'; binding.pry
+    if params[:destination].nil? || params[:destination] == ""
+      render json: ErrorSerializer.new("No location provided").bad_request, status: 400
+    else
+      salary_info_for_destination = SalariesFacade.new(params[:destination])
+      render json: SalariesSerializer.new(salary_info_for_destination.salaries_for_destination)
+    end
   end
 end
--- app/facades/salaries_facade.rb
+++ app/facades/salaries_facade.rb
@@ -1,32 +1,19 @@
 class SalariesFacade 
   attr_reader :destination
+  
   def initialize(destination)
     @destination = destination
   end
 
   def salaries_for_destination 
-    # forecast_info_destination = ForecastFacade.new(@destination) 
-    # lat_long = forecast_info_destination.lat_long
-    # weather_for_destination = WeatherService.new.fetch_forecast_for_given_location(lat_long.latitude, lat_long.longitude)
     weather_for_destination = fetch_weather
     forecast = {"summary": weather_for_destination[:current][:condition][:text], "temperature": "#{weather_for_destination[:current][:temp_f].to_i} F"} 
-   
-
-    # conn = Faraday.new(url: "https://api.teleport.org")
-    # urban_city_response = conn.get("/api/urban_areas/slug:#{@destination}/salaries")
-    # parsed =JSON.parse(urban_city_response.body, symbolize_names: true)
 
     parsed = salaries_service.salaries_for_destination(@destination)
 
-    tech_jobs = ["DATA-ANALYST", "DATA-SCIENTIST", "MOBILE-DEVELOPER", "QA-ENGINEER", "SOFTWARE-ENGINEER", "SYSTEMS-ADMINISTRATOR", "WEB-DEVELOPER"]
-   
-    wanted_list = parsed[:salaries].reject! do |job|
-      !tech_jobs.include?(job[:job][:id])
-    end
+    wanted_job_titles = select_jobs(parsed)
 
-    tech_salaries = wanted_list.map do |job|
-      SalaryInfo.new(job)
-    end
+    tech_salaries = create_salary_objects(wanted_job_titles)
 
     salaries = tech_salaries.map do |salary|
       {"title": salary.title, "min": salary.min, "max": salary.max}
@@ -43,5 +30,18 @@ class SalariesFacade
 
   def salaries_service 
     @salaries_service ||= SalariesService.new
+  end
+
+  def select_jobs(salary_info_collection)
+    tech_jobs = ["DATA-ANALYST", "DATA-SCIENTIST", "MOBILE-DEVELOPER", "QA-ENGINEER", "SOFTWARE-ENGINEER", "SYSTEMS-ADMINISTRATOR", "WEB-DEVELOPER"]
+    wanted_list = salary_info_collection[:salaries].reject! do |job|
+      !tech_jobs.include?(job[:job][:id])
+    end
+  end
+
+  def create_salary_objects(job_titles)
+    tech_salaries = job_titles.map do |job|
+      SalaryInfo.new(job)
+    end
   end
 end
