f04fb726297b5aebba2629cbe9c1ef9182c96656
KaraJH <karas.jones@yahoo.com>
2023-04-24 11:33:43 -0600

    test: Add assertions

--- spec/facades/salaries_facade_spec.rb
+++ spec/facades/salaries_facade_spec.rb
@@ -36,5 +36,27 @@ RSpec.describe SalariesFacade do
       expect(forecast[:current]).to have_key(:temp_f)
       expect(forecast[:current][:temp_f]).to be_a(Float)
     end
+
+    it "can get a list of only tech jobs" do 
+      attrs = {salaries: [
+                      {job: {id: "DATA-ANALYST", title: "Data Analyst"}, salary_percentiles: {percentile_25: 123.456, percentile_75: 567.89}},
+                      {job: {id: "COTTON-CANDY-MAKER", title: "Cotton Candy Maker"}, salary_percentiles: {percentile_25: 12.56, percentile_75: 56.89}}
+              ]}
+      selected = @facade.select_jobs(attrs)
+      expect(selected).to eq([{job: {id: "DATA-ANALYST", title: "Data Analyst"}, salary_percentiles: {percentile_25: 123.456, percentile_75: 567.89}}])
+    end
+
+    it "creates objects from the salary hash" do 
+      attrs = [{job: {id: "DATA-ANALYST", title: "Data Analyst"}, salary_percentiles: {percentile_25: 123.456, percentile_75: 567.89}},
+               {job: {id: "DATA-SCIENTIST", title: "Data Scientist"}, salary_percentiles: {percentile_25: 123.456, percentile_75: 567.89}}
+              ]
+      created_salary_objects = @facade.create_salary_objects(attrs)
+
+      expect(created_salary_objects.first).to be_a(SalaryInfo)
+      expect(created_salary_objects.last).to be_a(SalaryInfo)
+      expect(created_salary_objects.first.title).to eq("Data Analyst")
+      expect(created_salary_objects.first.min).to eq("$123.46")
+      expect(created_salary_objects.first.max).to eq("$567.89")
+    end
   end
 end
