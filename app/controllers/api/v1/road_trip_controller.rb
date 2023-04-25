ec00a44880b2e9867df1385f7656ea83f120fe2a
KaraJH <karas.jones@yahoo.com>
2023-04-24 19:10:02 -0600

    fix: Cleans up controllers

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::RoadTripController < ApplicationController
   def create 
     if !params[:api_key].present? || !User.all_keys.include?(params[:api_key])
        render json: ErrorSerializer.new("Key is incorrect or not present").bad_request, status: 401 
+    elsif !params[:origin].present? || !params[:destination].present?
+      render json: ErrorSerializer.new("Origin and/or destination was not provided").bad_request, status: 400
     else
       road_trip = RoadTripFacade.new(params[:origin], params[:destination]).road_trip
     
