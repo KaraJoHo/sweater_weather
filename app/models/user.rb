99efd4fd6a7b094d86eedbc8097a38443e50864f
KaraJH <karas.jones@yahoo.com>
2023-04-24 19:10:58 -0600

    fix: Fix spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates_presence_of :email, :password
   validates_uniqueness_of :email
   validates :password, confirmation: true
-
+ 
   has_secure_password
 
   def self.all_keys 
