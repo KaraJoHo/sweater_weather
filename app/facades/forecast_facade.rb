a4d8c4fc954e59455c2be6a6b6357da1945bc2c9
KaraJH <karas.jones@yahoo.com>
2023-04-24 19:10:19 -0600

    fix: Makes new helpers

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,15 +4,9 @@ class ForecastFacade
   end
 
   def lat_long 
-    # map_conn = Faraday.new(url: "https://www.mapquestapi.com/geocoding/v1/address")
-
-    # lat_long_response = map_conn.get("?key=#{ENV['MAPQUEST_KEY']}&location=#{@location}")
-    
-    # parsed = JSON.parse(lat_long_response.body, symbolize_names: true)
-   
     lat_long_hash = mapquest_service.lat_long(@location)[:results][0][:locations][0][:latLng]
    
-    lat_long = Geocode.new(lat_long_hash) #Geocode.new(parsed[:results][0][:locations][0][:latLng])
+    lat_long = Geocode.new(lat_long_hash) 
   end
 
   def mapquest_service 
@@ -21,14 +15,8 @@ class ForecastFacade
 
   def forecast_for_given_location 
 
-    # weather_conn = Faraday.new(url: "http://api.weatherapi.com")
-
-    # weather_response = weather_conn.get("/v1/forecast.json?key=#{ENV['WEATHER_KEY']}&q=#{lat_long.latitude},#{lat_long.longitude}&days=5")
-
-    # w_parsed = JSON.parse(weather_response.body, symbolize_names: true)
-    
     all_weather_info = weather_service.fetch_forecast_for_given_location(lat_long.latitude, lat_long.longitude)
-  
+
     current = CurrentWeather.new(all_weather_info[:current])
    
     five_day_forecast = all_weather_info[:forecast][:forecastday].map do |weather_info|
